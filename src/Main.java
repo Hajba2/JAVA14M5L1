a5e9007a5a78b1813c1c4713cf66f591219521ea
vokh <haiba.vova@gmail.com>
2023-06-14 19:23:36 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import car.Car;
+import human.Human;
+
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car();
+        //car.privateModel = "";
+        //car.pcgDefaultModel = "";
+        //car.protectedModel = "";
+        car.publicModel = "";
+    }
+}
---
+++ src/human/Tester.java
@@ -0,0 +1,14 @@
+package human;
+
+public class Tester extends Human {
+
+    private boolean isManual;
+
+    public boolean isManual() {
+        return isManual;
+    }
+
+    public void setManual(boolean manual) {
+        isManual = manual;
+    }
+}
